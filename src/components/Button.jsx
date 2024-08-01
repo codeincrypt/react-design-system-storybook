f1378e7815c4eb102b1ba84081f178cf93f5dc81
codeincrypt <kartikkr555@gmail.com>
2024-08-01 22:52:41 +0530

    project setup with storybook

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button as AntButton } from 'antd';
+
+const Button = (props) => {
+  return (
+    <AntButton {...props}></AntButton>
+  );
+};
+
+export default Button;
