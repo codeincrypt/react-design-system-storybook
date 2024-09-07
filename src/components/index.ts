1f13fd796c584ca31242595e674da6590ca6fbb4
codeincrypt <kartikkr555@gmail.com>
2024-09-08 00:55:53 +0530

    publish npm

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import Button from './Button';
+import Breadcrumb from './Breadcrumb';
+import Input from './Input';
+import Paragraph from './Paragraph';
+import Select from './Select';
+
+export {
+  Button,
+  Breadcrumb,
+  Input,
+  Paragraph,
+  Select
+}
